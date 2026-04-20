8bef49ce57fd5179897e30fee06184599fb52677
RJ <38520088+ryoju-ohata@users.noreply.github.com>
2026-04-20 15:49:38 +0900

    fix(speedtest): use average color mosaic algorithm and update checklist

--- 4_2024_speedtest/implementation/C11/index.php
+++ 4_2024_speedtest/implementation/C11/index.php
@@ -1,6 +1,10 @@
 <?php
 
-$logo1 = imagecreatefromjpeg('./original.jpg');
-imagefilter($logo1, IMG_FILTER_PIXELATE, $_GET['cell_size'] ?? 50);
+$image = imagecreatefromjpeg('./original.jpg');
+$s = $_GET['cell_size'] ?? 50;
+$w = imagesx($image);
+$h = imagesy($image);
+$small = imagescale($image, $w / $s, $h / $s);
+$mosaic = imagescale($small, $w, $h, IMG_NEAREST_NEIGHBOUR);
 header('Content-Type: image/jpeg');
-imagejpeg($logo1);
+imagejpeg($mosaic);
